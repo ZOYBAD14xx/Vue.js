901195ae75411aa63a603bdf4dfd31407330a3f2
ZOYBAD14xx <60214442+ZOYBAD14xx@users.noreply.github.com>
2020-09-21 12:44:40 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <!-- interpolation -->
+    <h1>{{title}}</h1>    
+    <h2>{{count}}</h2>
+    <button @click="clickAdd">Add</button>
+  </div>
+</template>
+<script>
+export default {
+  name:"app",
+  data() {
+    return {
+      title:"Number",
+      count:0,
+    }
+  },
+  methods: {
+    clickAdd(){
+      this.count=this.count+1
+    }
+  },
+};
+</script>
+<style></style>
